b3b87ad8910dadd3821311335effab7527a921a7
Donghee Kim <kimdhoe1@gmail.com>
2018-09-30 11:05:44 +0900

    init

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import Header from '../components/header'
+
+export default () => (
+  <Layout>
+    <Link to="/">Home</Link>
+    <Header headerText="Contact" />
+    <p>Send us a message!</p>
+  </Layout>
+)
